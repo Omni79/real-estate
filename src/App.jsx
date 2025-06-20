2bbfcf103908940647bbcd0cf52719726a4ae386
OMNI <omniframes79@gmail.com>
2025-06-20 22:03:25 +0100

    fourth commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
+import Header from "./components/Header";
 
 const App = () => {
   return (
-    <div>
-      
+    <div className="w-full overflow-hidden">
+      <Header />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
